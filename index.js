c08278cafd562ffae2e79c2359e5720032f9f068
inthemonzon <inthemonzon@yahoo.com>
2021-07-19 12:29:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function superbowlWin(record){
+    const WinningSeason = record.find(function(season){
+        return season.result === "W"
+    })
+    if (WinningSeason){
+    return WinningSeason.year;
+}
+}
+
+
+
 
